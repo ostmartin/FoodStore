b1cc3544585ed413744ddec7c1610352d1ce7d20
Arseniy2403 <arseniyermoshkin2403@gmail.com>
2023-07-24 16:47:39 +0300

    added fetch instead xml request

--- js/script.js
+++ js/script.js
@@ -244,30 +244,29 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            })
+            // const object = {};
+            // formData.forEach(function(value, key) {
+            //     object[key] = value;
+            // })
 
-            const jsonData = JSON.stringify(object);
+            // const jsonData = JSON.stringify(object);
 
-            request.send(jsonData);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                // headers: {
+                //     'Content-type': 'application/json'
+                // },
+                body: formData
+            }).then(data => {
+                console.log(data)
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset()
             })
         })
     }
