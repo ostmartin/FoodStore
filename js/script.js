5e70fae29f2a8fd0cf4d1c6c41a6ae7ef9d18f70
Arseniy2403 <arseniyermoshkin2403@gmail.com>
2023-07-25 12:24:46 +0300

    integrated axios, changed menu card adding

--- js/script.js
+++ js/script.js
@@ -193,22 +193,43 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
+    /* GET by using AXIOS lib */
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+    /*-------------------------------*/
 
-        return await res.json();
-    };
-
-    getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+    axios.get('http://localhost:3000/menu')
+    .then(res => {
+            res.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             })
         })
+
+    /* GET by native JS*/
+
+    /* -----------------------------*/
+
+    // const getResource = async (url) => {
+    //     const res = await fetch(url);
+
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
+
+    //     return await res.json();
+    // };
+
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         })
+    //     })
+
+
+
+    /* ONE-TIME CREATION EXAMPLE */
+
+    /*---------------------------------*/
 
     // getResource('http://localhost:3000/menu')
     // .then(data => createCard(data));
